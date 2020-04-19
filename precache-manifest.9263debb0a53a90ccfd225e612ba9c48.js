7294ba85a4552e65c04c2b3bd2e7038ed08a85bf
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2020-04-19 21:13:19 +0900

    Updates

---
+++ precache-manifest.9263debb0a53a90ccfd225e612ba9c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea6b0a11fddc7ea66d504456641e63d",
+    "url": "/gstp/index.html"
+  },
+  {
+    "revision": "b67bc4a39bfa952568cd",
+    "url": "/gstp/static/js/2.71df0a66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gstp/static/js/2.71df0a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d2da16c46114073eb8",
+    "url": "/gstp/static/js/main.76435d7b.chunk.js"
+  },
+  {
+    "revision": "6a7e2f62960ccd5b1199",
+    "url": "/gstp/static/js/runtime-main.34baa5b5.js"
+  }
+]);
