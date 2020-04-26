4879c616cb0cf8bc1cf24b873ad9a4d18caaf0c4
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2020-04-26 20:40:09 +0900

    Updates

---
+++ precache-manifest.8876911a2d20770fb3a788ebf98b4e9f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71834adb94bdd289be56d813dbc59daa",
+    "url": "/gstp/index.html"
+  },
+  {
+    "revision": "d14864c3124ae8431348",
+    "url": "/gstp/static/js/2.4a804f3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gstp/static/js/2.4a804f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462d8f690ead24590840",
+    "url": "/gstp/static/js/main.d76f65e0.chunk.js"
+  },
+  {
+    "revision": "6a7e2f62960ccd5b1199",
+    "url": "/gstp/static/js/runtime-main.34baa5b5.js"
+  },
+  {
+    "revision": "63086250f98b7106fc7dd9c81d35fc10",
+    "url": "/gstp/static/media/img-kakaotalk-logo.63086250.png"
+  },
+  {
+    "revision": "e1e425d8cc731be4150d037137022851",
+    "url": "/gstp/static/media/img-map.e1e425d8.jpg"
+  },
+  {
+    "revision": "ac6415c1d6416d3b24726398ab4c42f6",
+    "url": "/gstp/static/media/img-wechat-logo.ac6415c1.png"
+  },
+  {
+    "revision": "041d293535f0be1f8cfebf3ec256cdc4",
+    "url": "/gstp/static/media/logo-footer.041d2935.png"
+  },
+  {
+    "revision": "e97a2ef8e7fafcf970d840491ff78ca2",
+    "url": "/gstp/static/media/logo-thg-d.e97a2ef8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc40e55379bcc590dee82bd918f7743c.js"
+  "/gstp/precache-manifest.8876911a2d20770fb3a788ebf98b4e9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gstp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
